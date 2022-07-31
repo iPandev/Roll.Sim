442f5b4c9e6ea8bb74df13a1d320298845115440
iPandev <109651195+iPandev@users.noreply.github.com>
2022-07-31 09:24:49 -0700

    Annotated and cleaned up function

--- Func_virtual_track_conversion.py
+++ Func_virtual_track_conversion.py
@@ -1,37 +1,23 @@
 # A function to convert the spring rate and all 4 damper rates of all 4 wheels, to the at-wheel spring rates and at-wheel damper rates calcualted at a virtual track width, which is the average of the front and rear track widths.
+#TODO: validate Karb calculations
 
-def virtual_track_conversion(tw_f, tw_r,
-    f_wheel_rate,
-    r_wheel_rate,
-    f_slow_bump_rate,
-    f_slow_rebound_rate,
-    f_fast_bump_rate,
-    f_fast_rebound_rate,
-    r_slow_bump_rate,
-    r_slow_rebound_rate,
-    r_fast_bump_rate,
-    r_fast_rebound_rate):
+def RSF_virtual_track(tw_f, tw_r, #track widths
+    Ks_f, Ks_r, Karb_f, Karb_r, #coil and anti-roll bar spring rates
+    Csb_f, Csr_f, Cfb_f, Cfr_f, #front dampers
+    Csb_r, Csr_r, Cfb_r, Cfr_r): #rear dampers
 
-    tw_v = (tw_f + tw_r)/2
-    f_wheel_rate_virtual = f_wheel_rate*((tw_f/tw_v)**2)
-    r_wheel_rate_virtual = r_wheel_rate*((tw_r/tw_v)**2)
-    f_slow_bump_rate_virtual = f_slow_bump_rate*((tw_f/tw_v)**2)
-    f_slow_rebound_rate_virtual = f_slow_rebound_rate*((tw_f/tw_v)**2)
-    f_fast_bump_rate_virtual = f_fast_bump_rate*((tw_f/tw_v)**2)
-    f_fast_rebound_rate_virtual = f_fast_rebound_rate*((tw_f/tw_v)**2)
-    r_slow_bump_rate_virtual = r_slow_bump_rate*((tw_r/tw_v)**2)
-    r_slow_rebound_rate_virtual = r_slow_rebound_rate*((tw_r/tw_v)**2)
-    r_fast_bump_rate_virtual = r_fast_bump_rate*((tw_r/tw_v)**2)
-    r_fast_rebound_rate_virtual = r_fast_rebound_rate*((tw_r/tw_v)**2)
+    tw_v = (tw_f + tw_r)/2 #avg track width is virtual track width
 
-    return(tw_v,
-    f_wheel_rate_virtual,
-    r_wheel_rate_virtual, 
-    f_slow_bump_rate_virtual,
-    f_slow_rebound_rate_virtual,
-    f_fast_bump_rate_virtual,
-    f_fast_rebound_rate_virtual,
-    r_slow_bump_rate_virtual,
-    r_slow_rebound_rate_virtual,
-    r_fast_bump_rate_virtual,
-    r_fast_rebound_rate_virtual)
+    return((tw_f + tw_r)/2, #tw_v
+    Ks_f*((tw_f/tw_v)**2), #Ks_f_v
+    Ks_r*((tw_r/tw_v)**2), #Ks_r_v
+    Karb_f*((tw_f/tw_v)**2), #Karb_f_v
+    Karb_r*((tw_r/tw_v)**2), #Karb_r_v
+    Csb_f*((tw_f/tw_v)**2), #Csb_f_v
+    Csr_f*((tw_f/tw_v)**2), #Csr_f_v 
+    Cfb_f*((tw_f/tw_v)**2), #Cfb_f_v
+    Cfr_f*((tw_f/tw_v)**2), #Cfr_f_v
+    Csb_r*((tw_r/tw_v)**2), #Csb_r_v
+    Csr_r*((tw_r/tw_v)**2), #Csr_r_v
+    Cfb_r*((tw_r/tw_v)**2), #Cfb_r_v
+    Cfr_r*((tw_r/tw_v)**2)) #Cfr_r_v
